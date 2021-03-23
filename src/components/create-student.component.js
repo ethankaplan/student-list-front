77735391d007ce6ad5fc439a856fcb5533056f93
ethankaplan <phaladone@gmail.com>
2021-03-23 19:14:21 -0400

    added create and view students functionality

--- src/components/create-student.component.js
+++ src/components/create-student.component.js
@@ -1,6 +1,7 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button';
+import axios from 'axios';
 
 export default class CreateStudent extends Component {
 
@@ -22,26 +23,29 @@ export default class CreateStudent extends Component {
   }
 
   onChangeStudentName(e) {
-    this.setState({name: e.target.value})
+    this.setState({ name: e.target.value })
   }
 
   onChangeStudentEmail(e) {
-    this.setState({email: e.target.value})
+    this.setState({ email: e.target.value })
   }
 
   onChangeStudentRollno(e) {
-    this.setState({rollno: e.target.value})
+    this.setState({ rollno: e.target.value })
   }
 
   onSubmit(e) {
     e.preventDefault()
 
-    console.log(`Student successfully created!`);
-    console.log(`Name: ${this.state.name}`);
-    console.log(`Email: ${this.state.email}`);
-    console.log(`Roll no: ${this.state.rollno}`);
+    const studentObject = {
+      name: this.state.name,
+      email: this.state.email,
+      rollno: this.state.rollno
+    };
+    axios.post('http://localhost:4000/students/create-student', studentObject)
+      .then(res => console.log(res.data));
 
-    this.setState({name: '', email: '', rollno: ''})
+    this.setState({ name: '', email: '', rollno: '' })
   }
 
   render() {
@@ -49,17 +53,17 @@ export default class CreateStudent extends Component {
       <Form onSubmit={this.onSubmit}>
         <Form.Group controlId="Name">
           <Form.Label>Name</Form.Label>
-          <Form.Control type="text" value={this.state.name} onChange={this.onChangeStudentName}/>
+          <Form.Control type="text" value={this.state.name} onChange={this.onChangeStudentName} />
         </Form.Group>
 
         <Form.Group controlId="Email">
           <Form.Label>Email</Form.Label>
-          <Form.Control type="email" value={this.state.email} onChange={this.onChangeStudentEmail}/>
+          <Form.Control type="email" value={this.state.email} onChange={this.onChangeStudentEmail} />
         </Form.Group>
 
         <Form.Group controlId="Name">
           <Form.Label>Roll No</Form.Label>
-          <Form.Control type="text" value={this.state.rollno} onChange={this.onChangeStudentRollno}/>
+          <Form.Control type="text" value={this.state.rollno} onChange={this.onChangeStudentRollno} />
         </Form.Group>
 
         <Button variant="danger" size="lg" block="block" type="submit">
