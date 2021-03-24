77e188a5e7751fb69a5688dd25bbd1d93737da29
ethankaplan <phaladone@gmail.com>
2021-03-24 17:42:22 -0400

    all working

--- src/components/StudentTableRow.js
+++ src/components/StudentTableRow.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import Button from 'react-bootstrap/Button';
 
 export default class StudentTableRow extends Component {
+
+    constructor(props) {
+        super(props);
+        this.deleteStudent = this.deleteStudent.bind(this);
+    }
+
+    deleteStudent() {
+        axios.delete('http://localhost:4000/students/delete-student/' + this.props.obj._id)
+            .then((res) => {
+                console.log('Student successfully deleted!')
+            }).catch((error) => {
+                console.log(error)
+            })
+    }
+
     render() {
         return (
             <tr>
@@ -13,7 +29,7 @@ export default class StudentTableRow extends Component {
                     <Link className="edit-link" to={"/edit-student/" + this.props.obj._id}>
                         Edit
                     </Link>
-                    <Button size="sm" variant="danger">Delete</Button>
+                    <Button onClick={this.deleteStudent} size="sm" variant="danger">Delete</Button>
                 </td>
             </tr>
         );
