5eed18772c276897388bd8e935ab87540042448f
ethankaplan <phaladone@gmail.com>
2021-05-11 10:56:53 -0400

    tutorial completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
@@ -7,65 +7,67 @@ import Col from "react-bootstrap/Col";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link, withRouter } from "react-router-dom";
 
-import CreateStudent from "./components/create-student.component";
-import EditStudent from "./components/edit-student.component";
-import StudentList from "./components/student-list.component";
+import CreateStudent from "./components/CreateStudent/create-student.component";
+import EditStudent from "./components/EditStudent/edit-student.component";
+import StudentList from "./components/StudentList/student-list.component";
 
-function App() {
-  return (<Router>
-    <div className="App">
-      <header className="App-header">
-        <Navbar bg="dark" variant="dark">
-          <Container>
+class App extends Component {
+  render(){
+    return (<Router>
+      <div className="App">
+        <header className="App-header">
+          <Navbar bg="dark" variant="dark">
+            <Container>
 
-            <Navbar.Brand>
-              <Link to={"/create-student"} className="nav-link">
-                React MERN Stack App
-              </Link>
-            </Navbar.Brand>
-
-            <Nav className="justify-content-end">
-              <Nav>
+              <Navbar.Brand>
                 <Link to={"/create-student"} className="nav-link">
-                  Create Student
+                  React MERN Stack App
                 </Link>
+              </Navbar.Brand>
+
+              <Nav className="justify-content-end">
+                <Nav>
+                  <Link to={"/create-student"} className="nav-link">
+                    Create Student
+                  </Link>
+                </Nav>
+
+                {/* <Nav>
+                  <Link to={"/edit-student/:id"} className="nav-link">
+                    Edit Student
+                  </Link>
+                </Nav> */}
+
+                <Nav>
+                  <Link to={"/student-list"} className="nav-link">
+                    Student List
+                  </Link>
+                </Nav>
               </Nav>
 
-              {/* <Nav>
-                <Link to={"/edit-student/:id"} className="nav-link">
-                  Edit Student
-                </Link>
-              </Nav> */}
+            </Container>
+          </Navbar>
+        </header>
 
-              <Nav>
-                <Link to={"/student-list"} className="nav-link">
-                  Student List
-                </Link>
-              </Nav>
-            </Nav>
-
-          </Container>
-        </Navbar>
-      </header>
-
-      <Container>
-        <Row>
-          <Col md={12}>
-            <div className="wrapper">
-              <Switch>
-                <Route exact path='/' component={CreateStudent} />
-                <Route path="/create-student" component={CreateStudent} />
-                <Route path="/edit-student/:id" component={EditStudent} />
-                <Route path="/student-list" component={StudentList} />
-              </Switch>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  </Router>);
+        <Container>
+          <Row>
+            <Col md={12}>
+              <div className="wrapper">
+                <Switch>
+                  <Route exact path='/' component={CreateStudent} />
+                  <Route path="/create-student" component={CreateStudent} />
+                  <Route path="/edit-student/:id" component={EditStudent} />
+                  <Route path="/student-list" component={StudentList} />
+                </Switch>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </Router>);
+  }
 }
 
-export default App;
+export default withRouter(App);
