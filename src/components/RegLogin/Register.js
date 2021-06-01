53d3510f937cc3ae0cac44810f8663465c2461ed
ethankaplan <phaladone@gmail.com>
2021-06-01 16:03:41 -0400

    Frontend seemingly done

--- src/components/RegLogin/Register.js
+++ src/components/RegLogin/Register.js
@@ -18,6 +18,7 @@ export default class Register extends Component{
         return(
             <div>
                 <Form className="reglog">
+                    <Form.Group controlId="formBasicName">
                     <Form.Label>Name</Form.Label>
                     <Row>
                         <Col>
@@ -27,6 +28,48 @@ export default class Register extends Component{
                         <Form.Control placeholder="Last name" />
                         </Col>
                     </Row>
+                    </Form.Group>
+                    <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Enter password" />
+                    </Form.Group>
+                    
+                    <fieldset>
+                        <Form.Group as={Row}>
+                        <Form.Label as="legend" column sm={2}>
+                            Account Type
+                        </Form.Label>
+                            <Col sm={10}>
+                                <Form.Check
+                                type="radio"
+                                label="Administrator"
+                                name="formHorizontalRadios"
+                                id="Admin"
+                                />
+                                <Form.Check
+                                type="radio"
+                                label="Teacher"
+                                name="formHorizontalRadios"
+                                id="Teach"
+                                />
+                                <Form.Check
+                                type="radio"
+                                label="Student"
+                                name="formHorizontalRadios"
+                                id="Stud"
+                                />
+                            </Col>
+                        </Form.Group>
+                </fieldset>
+
+                          
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
                 </Form>
 
             </div>
