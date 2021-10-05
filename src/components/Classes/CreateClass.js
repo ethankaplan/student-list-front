60f1c2c45f94c09562071658e60474ff2d4b0690
ethankaplan <phaladone@gmail.com>
2021-10-04 18:48:06 -0700

    class work

---
+++ src/components/Classes/CreateClass.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+import {Form, Button, Row, Col} from 'react-bootstrap'
+import axios from 'axios';
+
+export default class CreateClass extends Component {
+
+  constructor(props) {
+    super(props)
+
+    // Setting up functions
+    this.changeHandler = this.changeHandler.bind(this)
+    this.onSubmit = this.onSubmit.bind(this);
+
+    // Setting up state
+    this.state = {
+      teacher: '',
+      title:'',
+      teachList
+    }
+  }
+
+  componentDidMount(){
+      axios.get(`${process.env.REACT_APP_BACKEND_URL}/`)
+  }
+
+  changeHandler = e => {
+    this.setState({
+        [e.target.name]: e.target.value  
+    })
+  };
+
+
+  onSubmit(e) {
+    e.preventDefault()
+
+    const studentObject = {
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      email: this.state.email,
+      rollNum: this.state.rollNum
+    };
+    console.log(studentObject)
+    axios.post(`${process.env.REACT_APP_BACKEND_URL}/students/create-student`, studentObject)
+      .then(res => console.log(res.data));
+
+    this.setState({ firstName: '',lastName:'', email: '', rollNum: '' })
+  }
+
+
+  render() {
+    return (<div className="form-wrapper">
+      <h2 className="d-flex justify-content-center">Add New Student</h2>
+      <Form onSubmit={this.onSubmit}>
+        <Form.Group controlId="ClassTitle">
+        <Form.Label>Class Title</Form.Label>
+              <Form.Control onChange={e => this.changeHandler(e)} name="title" placeholder="Title" value={this.state.title}/>
+        </Form.Group>
+        <Form.Group controlId="Teacher">
+          <Form.Label>Teacher</Form.Label>
+          <Form.Control type="email" name="email" value={this.state.teacher} onChange={e => this.changeHandler(e)} />
+        </Form.Group>
+
+        <Form.Group controlId="Name">
+          <Row><Col><Form.Label>Roll Number</Form.Label></Col>
+          <Col className="d-flex justify-content-end"><Button size='sm' variant="info" onClick={this.generate}> Generate </Button></Col></Row>
+          <Form.Control type="text" disabled name="rollNum" value={this.state.rollNum} onChange={e => this.changeHandler(e)} />
+        </Form.Group>
+
+        <Button variant="danger" size="lg" block="block" type="submit">
+          Create Student
+        </Button>
+      </Form>
+    </div>);
+  }
+}
